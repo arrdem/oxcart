9cb0d5d550b6ff1220b6b57e9f0fc30984ce7561
arrdem <me@arrdem.com>
2014-05-21 16:49:55 -0500

    Add the license and some more docs

--- src/oxcart/pattern.clj
+++ src/oxcart/pattern.clj
@@ -1,5 +1,23 @@
-(ns oxcart.pattern)
+;;   Copyright (c) Reid McKenzie, Rich Hickey & contributors. The use
+;;   and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0
+;;   (http://opensource.org/licenses/eclipse-1.0.php) which can be
+;;   found in the file epl-v10.html at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be
+;;   bound by the terms of this license.  You must not remove this
+;;   notice, or any other, from this software.
 
+(ns oxcart.pattern
+  {:doc "Implements a number of pattern matching predicates and utility
+        functions over the clojure.tools.analyzer.jvm AST structure."
+   :added "0.0.1"
+   :author "Reid McKenzie"})
+
+
+;; TODO:
+;;  Depending on benchmarking and API changes it may make sense to
+;;  make core.match a dependency of this project rather than hard
+;;  coding datastructure dependant paths and equality checks.
 
 (defn def?
   "λ AST -> Boolean
@@ -19,4 +37,3 @@
   [ast]
   (when (def? ast)
     (:name ast)))
-
