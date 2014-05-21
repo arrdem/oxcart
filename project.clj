54c7f5879dcb33b20c4619306340a16c15b4912a
arrdem <me@arrdem.com>
2014-05-21 11:09:27 -0500

    Update the project.clj with basic information

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject oxcart "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject me.arrdem/oxcart "0.1.0-SNAPSHOT"
+  :description "An experimental optimizing compiler for Clojure code"
+  :url "http://github.com/arrdem/oxcart"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
