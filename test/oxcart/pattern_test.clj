47aa00081a59382090272eac054145e897a277c3
arrdem <me@arrdem.com>
2014-05-23 12:24:37 -0500

    Refactor out test utils into a new ns

--- test/oxcart/pattern_test.clj
+++ test/oxcart/pattern_test.clj
@@ -1,23 +1,8 @@
 (ns oxcart.pattern-test
-  (:refer-clojure :exclude [macroexpand-1 macroexpand])
   (:require [oxcart.pattern :refer :all]
-            [clojure.test :refer :all]
-            [clojure.tools.analyzer.jvm :as ana.jvm]
-            [clojure.tools.analyzer
-             :refer [macroexpand-1
-                     macroexpand]
-             :as ana]))
+            [oxcart.test-util :refer :all]
+            [clojure.test :refer :all]))
 
-(defmacro is-not [form]
-  `(is (not ~form)))
-
-(defmacro ast [mform]
-  `(binding [ana/macroexpand-1 ana.jvm/macroexpand-1
-             ana/create-var    ana.jvm/create-var
-             ana/parse         ana.jvm/parse
-             ana/var?          var?]
-     (ana.jvm/analyze (quote ~mform)
-                      (ana.jvm/empty-env))))
 
 (def foo
   (ast
