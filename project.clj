08d0cd7f1afc977906fa7e577cc84b91d22f59cd
arrdem <me@arrdem.com>
2014-05-28 17:30:30 -0500

    Use the latest analyzer

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/clojure            "1.5.1"]
-                 [org.clojure/tools.analyzer     "0.1.0-beta13"]
+                 [org.clojure/tools.analyzer     "0.1.0-SNAPSHOT"]
                  [org.clojure/tools.analyzer.jvm "0.1.0-SNAPSHOT"]
                  [org.clojure/tools.emitter.jvm  "0.0.1-SNAPSHOT"]
                  [org.clojure/tools.reader       "0.8.4"]
