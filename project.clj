26613b60bf9b22589fe41db9fdb5df0ad916c8fa
arrdem <me@arrdem.com>
2014-06-02 12:12:46 -0500

    Add timbre as a dep

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [org.clojure/tools.analyzer.jvm "0.1.0-SNAPSHOT"]
                  [org.clojure/tools.emitter.jvm  "0.0.1-SNAPSHOT"]
                  [org.clojure/tools.reader       "0.8.4"]
+                 [com.taoensso/timbre            "3.2.1"]
                  ]
   :injections [(set! *print-length* 10)
                (set! *print-level*  10)])
