304984f4bd4ef0f17214664095ef496f27c56f58
arrdem <me@arrdem.com>
2014-06-04 09:58:00 -0500

    Work around known TANAL/TEJVM bug
    At present TANAL/TEJVM can't handle the Clojure core correctly. This
    patch, while jank at best, works around this failure so that I can
    continue analyzing and testing against whole program ASTs including the
    Clojure core.

--- src/oxcart.clj
+++ src/oxcart.clj
@@ -117,13 +117,8 @@
                                                 (.name *ns*))))))))
 
              ;; Run the code for side-effects
-
-             ;; FIXME: Loading core
-             ;;   This will happly eval clojure.core code, which tends
-             ;;   to break things. Should probably get a patch to
-             ;;   escape reloading and breaking core until Oxcart is
-             ;;   self-hosting.
-             (when eval?
+             (when (and eval?
+                        (not (= 'clojure.core (.name *ns*))))
                (em.jvm/eval mform)))))))
 
 
