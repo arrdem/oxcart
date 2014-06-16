f710f99f8e2d9091a74ed547fdb76f3c8c0fe3f9
arrdem <me@arrdem.com>
2014-06-16 17:04:37 -0500

    Implement var as value analysis

--- src/oxcart/passes/defs.clj
+++ src/oxcart/passes/defs.clj
@@ -12,8 +12,8 @@
    :added "0.0.2"
    :author "Reid McKenzie"}
   (:require [oxcart.pattern :as pattern]
-            [oxcart.passes :refer [record-pass
-                                   update-modules]]
+            [oxcart.passes :refer :all]
+            [clojure.tools.analyzer.ast :as ast]
             [clojure.set :refer [union]]))
 
 
@@ -115,3 +115,42 @@
              (locate-consts-in-module   options)
              (locate-dynamics-in-module options))))
       (record-pass locate-defs)))
+
+
+(defn write-context
+  [node]
+  (-> node
+      (ast/prewalk
+       (fn [node]
+         (cond (= :invoke (:op node))
+               (assoc-in node [:fn :env ::context] :invoke)
+               true node)))))
+
+
+(defn locate-var-as-value
+  "λ Whole-AST → Options → Whole-AST
+
+  Walks the argument AST, computing taken as value data for each
+  var. Creates a mapping `:usage' at the whole program level being
+  {Var → (U :value :target)}.
+
+  A Var maps to :value if it is _ever_ taken as a value, and has
+  the :target value if and only if it occurs only as a target or not
+  at all."
+  [whole-ast options]
+  (let [whole-ast (-> whole-ast
+                      (require-pass locate-defs {})
+                      (update-forms write-context))
+        m         (transient {})]
+    (doseq [form (whole-ast->forms whole-ast)]
+      (doseq [{:keys [op var] :as node} (ast/nodes form)]
+        (when var
+          (assoc! m var
+                  (if (and (= :var op)
+                           (not (= :value (get m node)))
+                           (= (-> node :env ::context) :invoke))
+                    :target
+                    :value)))))
+    (-> whole-ast
+        (assoc :usage (persistent! m))
+        (record-pass locate-var-as-value))))
