accae0589b105cdd133e854d4b2f52336be57e79
arrdem <me@arrdem.com>
2014-07-15 17:48:23 -0500

    WIP

--- src/oxcart/emitter/core.clj
+++ src/oxcart/emitter/core.clj
@@ -13,154 +13,49 @@
    :author "Reid McKenzie"
    :added "0.0.12"}
   (:require [oxcart
-             [util :refer [var-name]]
-             [passes :as passes :refer [require-pass]]
+             [util :refer [var-name var->ns var->name]]
+             [passes :as passes :refer [require-pass whole-ast->forms]]
              [pattern :as pattern]]
             [oxcart.passes.tree-shake
              :refer [analyze-var-uses tree-shake analyze-var-dependencies]]
             [oxcart.passes.fn-reduction 
-             :refer [reduce-fn-arities]]))
+             :refer [reduce-fn-arities]]
+            [oxcart.passes.defs
+             :refer [locate-var-as-value]]
+            [clojure.tools.emitter.jvm.transform
+             :refer [-compile]]))
 
-(defn make-class
-  [name]
-  {:name       name
-   :attr       0
-   :super      0
-   :fields     []
-   :methods    {}
-   :interfaces #{}})
-
-(defn add-fn-field
-  [class child-class]
-  )
-
-(defn cinit-last-fn-field
-  [class child-class]
-  )
-
-(defn add-method
-  "Adds a new method with the given name to the given class, returning
-  the updated class."
-  [class name {:keys [op] :as method}]
-  {:pre [(= :method op)
-         (string? name)]}
-  )
-
-(defn class->tejvm-class
-  "Rewrites a class into the TEJVM format for a class, returning a
-  valid TEJVM class presumably for final bytecode emission."
-  [class]
-  )
-
-;;--------------------------------------------------------------------
-
-(defn -emit-as-method
-  "(λ Form → This) → (This, nil)"
-  [{:keys [op init var] :as form} this]
-  {:pre [(= op :def)
-         (= :fn (:op init))
-         (var? var)]}
-  (let [sym-name (var-name var)]
-    [(add-method this (name sym-name) (first init)) nil]))
-
-
-(defn -emit-as-class
-  "(λ Form → This) → (This, (Seq Class))
-
-  the `this' argument is ignored and passed through unmodified. The
-  first (and only) class in the returned sequence of classes is the
-  result of emitting the argument def form as a standalone class
-  with (potentially) multiple implementations of the invoke method."
-  [{:keys [init var] :as form} this]
-  (let [sym-name (var-name var)
-        gen-class (-> (str (namespace sym-name) "." (name sym-name))
-                      make-class)
-        gen-class (reduce #(add-method %1 "invoke" %2) gen-class (:methods init))]
-    [this [gen-class]]))
-
-
-(defn -emit-as-field
-  "(λ Form → This) → (This, (Seq Class))
-
-  Emits the argument def as a class, writing a new static public field
-  into the `this` class being an instance of this new class."
-  [{:keys [name op] :as form} this]
-  {:pre [(= op :def)
-         (not (nil? name))
-         (symbol? name)]}
-  (let [[this₁ [child :as helpers]] (-emit-as-class form this)]
-    [(-> this₁
-         (add-fn-field child)
-         (cinit-last-fn-field name child))
-     helpers]))
-
-
-(defn -emit-form
-  "(λ Form → This) → (This, (Seq Class))
-
-  FIXME:
-    Note that this will totally explode for anything that isn't a
-    fn. Defs of constants aren't supported yet."
-  [{:keys [op] :as form} this]
-  (cond (and (= :def op)
-             (= :fn  (-> form :init :op)))
-        (if (-> form :meta :static)
-          (-emit-as-method form this)
-          (-emit-as-field  form this))
-
-        true
-        (assert false "lolwut not yet supported")))
-
-
-(defn emit-form
-  "(λ (This . (Seq Class)) → Form) → (This . (Seq Class))
-
-  Helper function extracted from emit-aot
-
-  Emits a single form, returning a new partial definition of \"this\",
-  and a sequence of auxiliary classes. This partial definition of
-  \"this\" contains any fields, methods and initializer code required
-  for the above."
-  [[this classes] form]
-  (let [[this' helper-classes] (-emit-form form this)]
-    [this' (concat classes helper-classes)]))
-
-
-(defn emit-namespace
-  "(λ Namespace) → (This . (Seq Class))
-
-  Helper function extracted from emit-aot
-
-  Emits a single namespace by walking all forms (which can only be
-  defs), emitting each form, and accumulating a \"this\" being the
-  namespace class itself, and a sequence of helper classes. Returns a
-  sequence of classes, including the final \"self\" class and all
-  helpers."
-  [{:keys [name forms] :as namespace}]
-  (let [this           (make-class name)
-        classes        []
-        [this classes] (reduce emit-form [this classes] forms)
-        this           (select-keys this [:name :attr :super :fields :methods :interfaces])]
-    (conj classes this)))
-
-
-(defn compile-class
-  "(λ Class) → (Name . Bytecode"
-  []
-  nil)
-
+(defn var->class [x]
+  {:pre  [(var? x)]
+   :post [(string? %)]}
+  (str (var->ns x) "." (var->name x)))
 
 (defn write-class
-  "(λ (Class . Bytecodes)) → Nil
+  [classname bytecode])
 
-  Helper function which takes a Class and spits out a bytecode file
-  for side-effects. Returns nil."
-  [[classname bytecode]]
-  
-  nil)
+(defn dispatch-fn
+  []
+  )
 
+(defmulti -emit
+  "(λ AST) → OpSeq
 
-(defn emit-aot
+  Recursively translates the given AST to TEJVM bytecode, returning a
+  sequence of opcodes.
+
+  OpSeq is (Seq (Σ Op Opseq))"
+  dispatch-fn)
+
+(defn emit-class
+  "(λ Def-AST) → Class-AST
+
+  Transforms a def to a class, returning a TEJVM AST describing the
+  resulting class."
+  [ast]
+  {:pre [(pattern/def? ast)]}
+  )
+
+(defn emit
   "(λ Whole-AST → Options) → nil
 
   Emits a whole program with reference to a single emtry point as
@@ -175,13 +70,24 @@
   {:pre  [(var? entry)]
    :post [(nil? %)]}
   (let [whole-program (-> whole-program
-                          (require-pass tree-shake        {})
-                          (require-pass reduce-fn-arities {})
-                          (require-pass tree-shake        {})
-                          (require-pass analyze-var-uses  {}))
-        reach         (get whole-program :var-reached)
-        load          (->> entry reach (map #(. %1 ns)) (into #{}))
-        classes       (mapcat emit-namespace load)]
-    (doseq [c classes]
-      (-> c compile-class write-class)))
+                          (require-pass tree-shake          options)
+                          (require-pass reduce-fn-arities   options)
+                          (require-pass tree-shake          options)
+                          (require-pass analyze-var-uses    options)
+                          (require-pass locate-var-as-value options))
+        reach         (-> whole-program (get :var-reached) (get entry))
+        reach-defs    (->> whole-program
+                           whole-ast->forms
+                           (keep #(and (pattern/def? %)
+                                       (reach (pattern/def->var %)))))]
+    ;; compile & write the set of used classes
+    (doseq [ast reach-defs]
+      (let [{:keys [class-name] :as class-ast} (-> ast emit-class)
+            class-bc                           (-> class-ast -compile)]
+        (write-class class-name class-bc)))
+
+    (let [class-name (var->ns entry)
+          class-ast  {}
+          class-bc   (-> class-ast -compile)]
+      (write-class class-name class-bc)))
   nil)
