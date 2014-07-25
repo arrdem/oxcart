796d2b762d462ecaf87aee6232aeaa377f481ffe
arrdem <me@arrdem.com>
2014-07-25 17:23:56 -0500

    Add the call program from test.benchmark

---
+++ src/call.clj
@@ -0,0 +1,15 @@
+(ns call
+  (:gen-class))
+
+(defn vcall ^long [^long i]
+  (inc i))
+
+(defn bench [^long reps]
+  (loop [i 0
+         accum 0]
+    (when (< i reps)
+      (recur (inc i) (vcall accum)))))
+
+(defn -main [& args]
+  (dotimes [_ 10]
+    (time (bench 10000000))))
