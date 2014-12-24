58c3e2ba1227664b1749c6f3c65be8f2b319b0b2
arrdem <me@arrdem.com>
2014-12-24 14:41:26 -0600

    Sort pattern stuff

--- src/main/clojure/oxcart/pattern.clj
+++ src/main/clojure/oxcart/pattern.clj
@@ -11,7 +11,8 @@
 ;;  make core.match a dependency of this project rather than hard
 ;;  coding datastructure dependant paths and equality checks.
 
-
+;; Defs
+;;------------------------------------------------------------------------------
 (defn def?
   "λ AST -> Boolean
 
@@ -20,7 +21,6 @@
   (-> ast
       :op
       (= :def)))
-
 
 (defn def->symbol
   "λ AST -> (Option Symbol)
@@ -31,7 +31,6 @@
   (when (def? ast)
     (:name ast)))
 
-
 (defn def->var
   "λ AST → (Option Var)
 
@@ -41,121 +40,12 @@
   (when (def? ast)
     (:var ast)))
 
-
 (defn top-level?
   "λ AST → Boolean
 
   Indicates whether the argument AST is a top level form."
   [ast]
   (:top-level ast))
-
-
-(defn fn?
-  "λ AST → Boolean
-
-  Indicates whether the top level form of the argument AST is a fn."
-  [ast]
-  (-> ast :op (= :fn)))
-
-
-(defn fn->name
-  "λ AST → (Option Symbol)
-
-  If the argument AST is a fn, attempts to return the internal name of
-  the fn defaulting to a freshly generated symbol."
-  [ast]
-  (when (fn? ast)
-    (-> ast :internal-name)))
-
-
-(defn fn-method?
-  "λ AST → Boolean
-
-  Indicates whether the argument form is a fn-method."
-
-  [ast]
-  (-> ast :op (= :fn-method)))
-
-
-(defn let?
-  "λ AST → Boolean
-
-  Indicates whether the argument form is a let."
-  [ast]
-  (-> ast :op (= :let)))
-
-
-(defn letfn?
-  "λ AST → Boolean
-
-  Indicates whether the argument form is a letfn."
-  [ast]
-  (-> ast :op (= :letfn)))
-
-
-(defn binding?
-  "λ AST → Boolean
-
-  Indicates whether the argument form is a binding node."
-  [ast]
-  (-> ast :op (= :binding)))
-
-
-(defn binding->symbol
-  "λ AST → (Option Symbol)
-
-  If the argument form was a binding, returns the bound local
-  symbol."
-  [ast]
-  (when (binding? ast)
-    (-> ast :name)))
-
-
-(defn binding->value
-  "λ AST → (Option AST)
-
-  If the argument form was a binding, returns the bound value as an
-  AST node."
-  [ast]
-  (when (binding? ast)
-    (-> ast :init)))
-
-
-(defn local?
-  "λ AST → Boolean
-
-  Indicates whether the argument form is a local node."
-  [ast]
-  (-> ast :op (= :local)))
-
-
-(defn local->symbol
-  "λ AST → (Option symbol)
-
-  If the argument AST was a local node, then this operation returns
-  the name field otherwise nil."
-  [ast]
-  (when (local? ast)
-    (:name ast)))
-
-
-(defn invoke?
-  "λ AST → Boolean
-
-  Indicates whether the argument top level form is an invocation."
-  [ast]
-  (-> ast :op (= :invoke)))
-
-
-(defn invoke->fn
-  "λ AST → (Option AST)
-
-  If the argument AST was an invocation, returns the AST representing
-  the invoked value."
-  [ast]
-  (when (invoke? ast)
-    (-> ast :fn)))
-
 
 (defn private?
   "λ AST → bool
@@ -170,7 +60,6 @@
       (true? status)
       true)))
 
-
 (defn public?
   "λ AST → bool
 
@@ -181,7 +70,6 @@
   [form]
   (-> form private? not))
 
-
 (defn dynamic?
   "λ AST → bool
 
@@ -190,7 +78,6 @@
   definition, however this behavior is subject to change."
   [form]
   (-> form :meta :form :dynamic true?))
-
 
 (defn const?
   "λ AST → bool
@@ -203,3 +90,104 @@
   (case (-> form :meta :form :const)
     (true nil) (not (dynamic? form))
     :else      false))
+
+;; Fns
+;;------------------------------------------------------------------------------
+(defn fn?
+  "λ AST → Boolean
+
+  Indicates whether the top level form of the argument AST is a fn."
+  [ast]
+  (-> ast :op (= :fn)))
+
+(defn fn->name
+  "λ AST → (Option Symbol)
+
+  If the argument AST is a fn, attempts to return the internal name of
+  the fn defaulting to a freshly generated symbol."
+  [ast]
+  (when (fn? ast)
+    (-> ast :internal-name)))
+
+(defn fn-method?
+  "λ AST → Boolean
+
+  Indicates whether the argument form is a fn-method."
+
+  [ast]
+  (-> ast :op (= :fn-method)))
+
+;; Let
+;;------------------------------------------------------------------------------
+(defn let?
+  "λ AST → Boolean
+
+  Indicates whether the argument form is a let."
+  [ast]
+  (-> ast :op (= :let)))
+
+(defn letfn?
+  "λ AST → Boolean
+
+  Indicates whether the argument form is a letfn."
+  [ast]
+  (-> ast :op (= :letfn)))
+
+(defn binding?
+  "λ AST → Boolean
+
+  Indicates whether the argument form is a binding node."
+  [ast]
+  (-> ast :op (= :binding)))
+
+(defn binding->symbol
+  "λ AST → (Option Symbol)
+
+  If the argument form was a binding, returns the bound local
+  symbol."
+  [ast]
+  (when (binding? ast)
+    (-> ast :name)))
+
+(defn binding->value
+  "λ AST → (Option AST)
+
+  If the argument form was a binding, returns the bound value as an
+  AST node."
+  [ast]
+  (when (binding? ast)
+    (-> ast :init)))
+
+(defn local?
+  "λ AST → Boolean
+
+  Indicates whether the argument form is a local node."
+  [ast]
+  (-> ast :op (= :local)))
+
+(defn local->symbol
+  "λ AST → (Option symbol)
+
+  If the argument AST was a local node, then this operation returns
+  the name field otherwise nil."
+  [ast]
+  (when (local? ast)
+    (:name ast)))
+
+;; Invocations
+;;------------------------------------------------------------------------------
+(defn invoke?
+  "λ AST → Boolean
+
+  Indicates whether the argument top level form is an invocation."
+  [ast]
+  (-> ast :op (= :invoke)))
+
+(defn invoke->fn
+  "λ AST → (Option AST)
+
+  If the argument AST was an invocation, returns the AST representing
+  the invoked value."
+  [ast]
+  (when (invoke? ast)
+    (-> ast :fn)))
