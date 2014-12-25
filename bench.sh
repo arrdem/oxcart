2e3b6f99f60aa82187e4b5e4e8fa77c84d5ec1d1
arrdem <me@arrdem.com>
2014-12-24 18:00:52 -0600

    [CLEAN] Refactor to add an "all" command

--- bench.sh
+++ bench.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
-CP=`lein cp`
-OPTS="-cp $CP -server -XX:+TieredCompilation -XX:+AggressiveOpts -Xmx1024m"
-NS=$1
 
-echo "Running Clojure 1.6.0 compiled $NS...."
-time java $OPTS\
-     -cp $CP\
-     clojure.main\
-     -m $NS
+bench_one() {
+       CP=`lein cp`
+       OPTS="-cp $CP -server -XX:+TieredCompilation -XX:+AggressiveOpts -Xmx1024m"
+       NS=$1
 
-echo "Oxcart compiling $NS...." &&
-lein run $NS &> /dev/null &&
-echo "Running Oxcart compiled $NS...." &&
-time java $OPTS\
-     -cp $CP\
-     $NS
+       echo "Running Clojure 1.6.0 compiled $NS...."
+       time java $OPTS\
+            -cp $CP\
+            clojure.main\
+            -m $NS
+
+       echo "Oxcart compiling $NS...." &&
+           lein run $NS &> /dev/null &&
+           echo "Running Oxcart compiled $NS...." &&
+           time java $OPTS\
+                -cp $CP\
+                $NS
+}
+
+bench_all() {
+    for ns in `grep -h -r "ns" src/bench/ | awk '{print $2}' | sed 's/[()]//g'`;
+    do
+        bench_one "$ns"
+    done
+}
+
+case $1 in
+    "all")
+        bench_all
+        ;;
+    *)
+        bench_one $1
+        ;;
+esac
