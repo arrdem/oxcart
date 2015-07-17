3fbc55530f194fafce955cd04be7799fb60b1532
Reid 'arrdem' McKenzie <reid@factual.com>
2015-07-16 18:19:15 -0700

    Bump deps

--- project.clj
+++ project.clj
@@ -4,12 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure            "1.6.0"]
-                 [org.clojure/tools.analyzer     "0.6.4"]
-                 [org.clojure/tools.analyzer.jvm "0.6.5"]
-                 [org.clojure/tools.emitter.jvm  "0.1.0-SNAPSHOT"]
-                 [org.clojure/tools.reader       "0.8.13"]
-                 [com.taoensso/timbre            "3.3.1"]
+                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.analyzer.jvm "0.6.7"]
+                 [org.clojure/tools.reader "0.9.2"]
+                 [org.clojure/tools.reader "0.8.13"]
+                 [com.taoensso/timbre "3.3.1"]
                  ]
   :main oxcart.core
   :min-lein-version "2.0.0"
