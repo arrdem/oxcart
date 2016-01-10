630f0ede6118cc9c7ee7a9a763086c7ea2772126
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-01-10 12:24:54 -0600

    Fix whitespace

--- src/main/clojure/oxcart/core.clj
+++ src/main/clojure/oxcart/core.clj
@@ -234,8 +234,8 @@
 
 
 (defn load-ast
-  "(  Resource)   Whole-AST
-  (  Resource   Options)   Whole-AST
+  "(λ Resource) → Whole-AST
+  (λ Resource → Options) → Whole-AST
 
   Wrapper around the main load function which provides helper
   mechanics for the common case of wanting to load a resource and get
